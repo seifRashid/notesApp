52f89a464d562702cbbd1d7e4731a65f720ef300
seifRashid <rseif1218@gmail.com>
2024-12-31 05:15:31 +0300

    create notes page

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="flex gap-5 mx-3 items-center mt-3">
+      <img alt="Vue logo" class="size-8" src="@/assets/noteLogo.svg" />
+
+      <div class="wrapper">
+        <!-- <HelloWorld msg="You did it!" /> -->
+
+        <nav>
+          <div class="flex gap-5 items-center">
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="m-5">
+      <button
+      @click="console.log('hello')"
+        class="flex gap-1 bg-blue-500 py-2 px-3 rounded-lg text-white font-semibold border-blue-600"
+      >
+        <img alt="Vue logo" class="size-5" src="@/assets/buttonIcon.svg" />
+        Create Note
+      </button>
+    </div>
+  </main>
+</template>

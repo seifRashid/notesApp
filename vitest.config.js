edfe2a4b4784a828b0e695c49658773949590d9c
seifRashid <rseif1218@gmail.com>
2025-01-01 17:32:33 +0300

    adding dist

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base:'/notesApp/',
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
